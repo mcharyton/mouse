805cb65a27ec788cecf5dfdc560494b9b34edb34
mcharyton <charytom@ee.pw.edu.pl>
2016-06-17 18:34:14 +0200

    joy

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/api', api);
 app.use('/joystick', joystick);
 app.use('/tilt', tilt);
 app.get('/joy', function(req,res){
-  res.sendfile(__dirname + '/public/joy.html');
+  res.sendFile(__dirname + '/public/joy.html');
 });
 
 // catch 404 and forward to error handler
