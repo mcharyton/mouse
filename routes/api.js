e5e440eed3bd61f7045a46c6584bcc0a9aaae21c
mcharyton <charytom@ee.pw.edu.pl>
2016-06-17 15:03:12 +0200

    api send update

--- routes/api.js
+++ routes/api.js
@@ -6,11 +6,17 @@ var app = express();
 var expressWs = require('express-ws')(app);
 var router = express.Router();
 
+
 /* POST users listing. */
 router.post('/send', function(req, res) {
 //    ws.send(req.body);
     ws.send('test');
 
+});
+router.ws('/send',function(ws,req){
+    ws.on('message',function(msg){
+        console.log(msg);
+    });
 });
 
 module.exports = router;
