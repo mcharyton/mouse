6a6afaf0ec6e40aac88ba97aa7f992e05120ba63
mcharyton <charytom@ee.pw.edu.pl>
2016-06-17 20:41:47 +0200

    Data stringified

--- routes/api.js
+++ routes/api.js
@@ -22,8 +22,8 @@ router.get('/', function(req,res){
 router.post('/', function(req,res){
    // event.emit('koko');
     var body = req.body;
-  //  if(body.x!=='0' && body.y!=='0')
-        emituj(body);
+    if(body.x!=='0' && body.y!=='0')
+        emituj(json.stringify(body));
 
     res.send('POST handler for api');
     res.end('Odpowiedzialem');
