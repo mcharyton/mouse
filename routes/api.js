735368e5253d5d6281f0cc5db30119304a1b5e1d
mcharyton <charytom@ee.pw.edu.pl>
2016-06-17 17:58:05 +0200

    Emit test

--- routes/api.js
+++ routes/api.js
@@ -12,33 +12,26 @@ var server = http.createServer(app);
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({server: server});
 
-wss.on('connection', function connection(ws) {
-    console.log('client connected');
-});
-
 router.get('/', function(req,res){
     event.emit('koko');
     console.log("Received "+req.body);
 });
 
-
-app.post('/', function(req,res){
+router.post('/', function(req,res){
+    event.emit('koko');
     console.log("Received "+req.body);
-    wss.send(req.body);
-    res.status(200);
-    res.end();
 });
 
 wss.on('connection', function(ws) {
-    ws.on('message', function(message) {
-        console.log('r : %s', message);
-    });
+    console.log('client connected');
+    //  ws.on('message', function(message) {
+    //    console.log('r : %s', message);
+    //  });
 
     // listen the event
-    event.on('homePage', function(){
-        ws.emit('someEvent');
+    event.on('koko', function(){
+        ws.send('cos');
     });
 });
-
 
 module.exports = router;
