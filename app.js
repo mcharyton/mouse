8ef2bd589d7fe46d76e3645e27ad82cc0cfb6cbc
mcharyton <charytom@ee.pw.edu.pl>
2016-06-17 12:55:36 +0200

    WebSocket added

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 var express = require('express');
+var app = express();
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+var dbN = 'mouse';
+var mongoUrl = 'mongodb://localhost/'+dbN;
+mongoose.connect(mongoUrl);
+
+var db = mongoose.connection;
+db.on('error',console.error.bind(console, 'mongo connection error:'));
+db.once('open',function(){
+  console.log("Connected correctly to mongo server db: " + dbN);
+});
+
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var api = require('./routes/api');
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,12 +66,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-
-var server = app.listen(8081, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port)
-
-})
