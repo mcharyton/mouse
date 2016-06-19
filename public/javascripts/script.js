6a5959cf810a5882b3e4f0222c82759e158fbf6d
mcharyton <charytom@ee.pw.edu.pl>
2016-06-19 23:41:34 +0200

    changed emitter on to once

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -13,35 +13,35 @@ function handleOrientation(event) {
   // do nothing if last move was less than 100 ms ago
   if(Date.now() - lastMove > 10) {
 
-  var x = event.gamma;  // In degree in the range [-180,180]
-  var y = event.beta; // In degree in the range [-90,90]
+    var x = event.gamma;  // In degree in the range [-180,180]
+    var y = event.beta; // In degree in the range [-90,90]
 
 
-  // Because we don't want to have the device upside down
-  // We constrain the x value to the range [-90,90]
-  if (x >  90) { x =  90};
-  if (x < -90) { x = -90};
+    // Because we don't want to have the device upside down
+    // We constrain the x value to the range [-90,90]
+    if (x >  90) { x =  90};
+    if (x < -90) { x = -90};
 
-  // To make computation easier we shift the range of 
-  // x and y to [0,180]
-  x += 90;
-  y += 90;
-  var xInt = Math.floor(x)-90;
-  var yInt = Math.floor(y)-90;
+    // To make computation easier we shift the range of
+    // x and y to [0,180]
+    x += 90;
+    y += 90;
+    var xInt = Math.floor(x)-90;
+    var yInt = Math.floor(y)-90;
 
-  output.innerHTML  = "beta : " + xInt + "\n";
-  output.innerHTML += "gamma: " + yInt + "\n";
-  var data = {type: "touch", x: xInt, y: yInt};
-  //if(joystick.deltaX()!==0 && joystick.deltaY()!==0)
-  if(xInt!=lastX || yInt!==lastY)
-    sendAjax(data);
-  // 10 is half the size of the ball
-  // It center the positioning point to the center of the ball
-  ball.style.top  = (maxX*x/180 - 10) + "px";
-  ball.style.left = (maxY*y/180 - 10) + "px";
-  lastY = yInt;
-  lastX = xInt;
-    lastMove = Date.now();
+    output.innerHTML  = "beta : " + xInt + "\n";
+    output.innerHTML += "gamma: " + yInt + "\n";
+    var data = {type: "touch", x: xInt, y: yInt};
+    //if(joystick.deltaX()!==0 && joystick.deltaY()!==0)
+    if(xInt!=lastX || yInt!==lastY)
+      sendAjax(data);
+    // 10 is half the size of the ball
+    // It center the positioning point to the center of the ball
+    ball.style.top  = (maxX*x/180 - 10) + "px";
+    ball.style.left = (maxY*y/180 - 10) + "px";
+    lastY = yInt;
+    lastX = xInt;
+      lastMove = Date.now();
   } else return
 
 }
