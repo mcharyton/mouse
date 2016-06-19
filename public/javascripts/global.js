d838cd27ee8e774bb110efc0fc4e534049802cfa
mcharyton <charytom@ee.pw.edu.pl>
2016-06-19 19:29:05 +0200

    ajax cleaned

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -2,24 +2,23 @@
  * Created by m.charyton on 07.06.2016.
  */
 function LButton() {
-    var data = { type: "btn", btn: "left" };
+    var data = {type: "btn", btn: "left"};
     sendAjax(data);
 }
 function DoubleLButton() {
-    var data = { type: "btn", btn: "doubleLeft" };
+    var data = {type: "btn", btn: "doubleLeft"};
     sendAjax(data);
 }
 function RButton() {
-    var data = { type: "btn", btn: "right" };
+    var data = {type: "btn", btn: "right"};
     sendAjax(data);
 }
 
 function sendAjax(data) {
     $.ajax({
         type: "POST",
-        url: "http://167.114.242.19/data",
+        url: "http://167.114.242.19/api",
         data: data,
         dataType: "json"
-
     });
 }
