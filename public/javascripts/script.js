5231d8a7a7c6e952c42e3d54eb6d934443538db7
mcharyton <charytom@ee.pw.edu.pl>
2016-06-20 00:32:17 +0200

    changed x y of arrow

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -37,8 +37,8 @@ function handleOrientation(event) {
       sendAjax(data);
     // 10 is half the size of the ball
     // It center the positioning point to the center of the ball
-    ball.style.top  = (maxX*x/180 - 10) + "px";
-    ball.style.left = (maxY*y/180 - 10) + "px";
+    ball.style.left  = (maxX*x/180 - 10) + "px";
+    ball.style.top= (maxY*y/180 - 10) + "px";
     lastY = yInt;
     lastX = xInt;
       lastMove = Date.now();
